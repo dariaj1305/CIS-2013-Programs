550c2fea1ffd71e4b5428db9e80e68d18fdbbb2e
dariaj1305 <89709502+dariaj1305@users.noreply.github.com>
2021-09-17 18:13:23 -0500

    Add files via upload

--- mpg.js
+++ mpg.js
@@ -3,18 +3,12 @@
   <meta charset="UTF-8">
   <title>Calculate Miles Per Gallon</title>
   <script src="mpg.js">
-    var floatMiles= parseFloat(prompt("Enter miles driven"));
-    var floatGallons= parseFloat(prompt("Enter gallons of gas used"));
-    var floatMPG= (miles/gallons);
-    alert("Miles per gallon= " + mpg);
-    var usersame = prompt("Please enter your name");
-    var age = parseInt(prompt(Please enter your age:", "18"));
-    var wage = parseFloat(
-    prompt("Please enter the hourly wage", "5.35"));
-    var age = prompt("Please enter your age:");
-if ( age = = = null ) {
-    alert("You clicked cancel.");
-}
+    var floatMiles= prompt("Enter miles driven");
+    floatMiles=parseFloat(floatMiles);
+    var floatGallons= prompt("Enter gallons of gas used");
+    floatGallons=parseFloat(floatGallons);
+    var floatMPG= (floatMiles/FloatGallons);
+    alert("Miles per gallon= " + floatMPG);
 
   </script>
 </head>
