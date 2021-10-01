db75f5c66da9c78845bf5484b342fc223c443767
dariaj1305 <89709502+dariaj1305@users.noreply.github.com>
2021-10-01 16:39:52 -0500

    Add files via upload

---
+++ Loan.js
@@ -0,0 +1,28 @@
+var $ = function (id)
+{
+    return document.getElementById(id);
+}
+
+/* start
+   declare floatMonthlyPayment, intLoanTerm, 	floatTotalCost
+   get floatMonthlyPayment
+   get intLoanTerm with loan term in months
+   calculate floatTotalCost = floatMonthlyPayment *      	intLoanTerm
+   output floatTotalCost
+end
+*/
+var product = "monthly_payment" * "loan_length";
+
+var floatMonthlyPayment, intLoanTerm, floatTotalCost
+var floatMonthlyPayment = parseFloat($("monthly_payment").value);
+var intLoanTerm = parseInt($("loan_length").value);
+var floatTotalCost = parseFloat(floatMonthlyPayment * intLoanTerm);
+var floatTotalCost= $("total_cost").value;
+
+window.onload = product
+{
+    $("loan_length").value = "";
+    $("total_cost").value = "";
+    $("calc").onclick = product;
+    $("monthly_payment").focus();
+}
